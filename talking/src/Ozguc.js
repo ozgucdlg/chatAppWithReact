343b53f017f06e5d3e846fc640cd4942b2170518
DALGA Ozguc <ozgucdalga@gmail.com>
2022-01-17 00:16:51 -0500

    react running

---
+++ talking/src/Ozguc.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function  Ozguc()  {
+    return (
+        <div>
+
+            <h2 style={{color: 'red'}}>HELLO FOLKS FOR CHATTING APP B USING RACT</h2>
+            
+        </div>
+    )
+}
+
+export default  Ozguc;
+
